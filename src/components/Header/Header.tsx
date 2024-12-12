177703b35b394b3811e19c09fda9478f4d755d66
mide6x <adewoleolumide05@gmail.com>
2024-12-12 20:06:09 +0100

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,18 @@
 import { Box, Container, Flex, Button, Image } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <Box as="header" py={4} bg="white" boxShadow="sm">
       <Container maxW="container.xl">
         <Flex justify="space-between" align="center">
-          <Flex align="center">
-            <Image
-              src="src/assets/ntype.png"
-              alt="Notype.ai logo"
-              h="24px"
-              mr={2}
-            />
+          <Flex
+            align="center"
+            as={RouterLink}
+            to="/"
+            _hover={{ textDecoration: "none" }}
+          >
+            <Image src="/ntype.png" alt="Notype.ai logo" h="24px" mr={2} />
             <Box fontSize="xl" fontWeight="bold">
               Notype.ai
             </Box>
